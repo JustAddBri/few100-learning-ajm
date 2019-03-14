b433d7e85319dd2445ba259dd0d6ca0910420ec2
JustAddBri <48483935+JustAddBri@users.noreply.github.com>
2019-03-14 17:01:24 +0000

    pre-lunch

--- spec/utils.ts
+++ spec/utils.ts
@@ -11,3 +11,5 @@ type Transformer = (x: string) => string;
 export function formatName(first: string, last: string, transformer: Transformer = (x) => x): string {
     return transformer(`${last}, ${first}`);
 }
+
+export const isEven = (n: number) => n % 2 === 0;
